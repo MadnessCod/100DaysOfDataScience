a6b2033f279a334b021da562baeb187ce373c959
Sadra Hosseini <sadrahosseini98@gmail.com>
2025-03-13 07:45:03 +0100

    found the problem in scraping quotes.toscrape.com, should have requested http not https

--- QuoteGuessingGame/src/QuoteGuessingGame/scraper.py
+++ QuoteGuessingGame/src/QuoteGuessingGame/scraper.py
@@ -1,7 +1,7 @@
 import asyncio
+
 from dataclasses import dataclass
 from typing import Literal
-
 
 import httpx
 
@@ -9,13 +9,20 @@ import httpx
 @dataclass
 class QuoteScraper:
     """
+    Class is used to scrapy quotes from http://quotes.toscrape.com/
+    :param url: Literal http://quotes.toscrape.com/
+    :type url: str
     """
     file : Literal['Quotes.json'] = 'Quotes.json'
-    url : Literal['https://quotes.toscrape.com/'] = 'https://quotes.toscrape.com/'
+    url : Literal['http://quotes.toscrape.com/'] = 'http://quotes.toscrape.com/'
 
     async def scrape(self, url) -> httpx.Response.text:
         """
-        :return:
+        send request to url
+        :param url: url to scrape
+        :type url: str
+        :return: httpx.Response.text
+        :rtype: str
         """
         async with httpx.AsyncClient() as client:
             try:
@@ -27,10 +34,11 @@ class QuoteScraper:
 
     async def increment_page(self) -> None:
         """
+        incrementing page number for http://quotes.toscrape.com/
         """
         counter = 1
         while True:
-            text = await self.scrape(url=f'{self.url}page/{counter}')
+            text = await self.scrape(url=f'{self.url}page/{counter}/')
             if text is None:
                 break
 
